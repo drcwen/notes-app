b5a7bec6c2b27a2f30e584eac93a963ea97489d1
Wendel Derraco <wendelderraco@gmail.com>
2025-12-11 01:06:41 +0800

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+
+//CREATE USER
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// READ ALL
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// READ ONE
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(404).json({ error: "User not found" });
+  }
+});
+
+module.exports = router;
